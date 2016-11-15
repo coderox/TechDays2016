67c78e2047e46fbf7a8e0df248c32a08af2297c4
Johan Lindfors <johan.lindfors@coderox.se>
2016-11-15 18:59:03 +0100

    Now with variable sizes

--- Client.Library/Views/SeventhPage.xaml.cs
+++ Client.Library/Views/SeventhPage.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 namespace Client.Views
 {
@@ -26,7 +14,7 @@ namespace Client.Views
             Loaded += delegate {
                 if (ActualWidth < 641) {
                     VisualStateManager.GoToState(this, "small", false);
-                } else if(ActualWidth < 1008) {
+                } else if (ActualWidth < 1008) {
                     VisualStateManager.GoToState(this, "medium", false);
                 } else {
                     VisualStateManager.GoToState(this, "large", false);
@@ -60,6 +48,19 @@ namespace Client.Views
             }
         }
 
+        private int columnSpan = 1;
+        public int ColumnSpan
+        {
+            get { return columnSpan; }
+            set
+            {
+                columnSpan = value;
+                if (PropertyChanged != null) {
+                    PropertyChanged(this, new PropertyChangedEventArgs("ColumnSpan"));
+                }
+            }
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         private void OnSizeChanged(object sender, SizeChangedEventArgs e)
@@ -67,23 +68,43 @@ namespace Client.Views
             var width = e.NewSize.Width;
             var height = e.NewSize.Height - appBar.ActualHeight;
 
-            var margin = 0;// width <= 640 ? 12 : 24;
             double iWidth = 0;
             double itemHeight = 0;
             if (maximumRowsOrColumns == 1) { // small
-                iWidth = width - (2 * margin);
+                iWidth = width;
                 itemHeight = Math.Floor(height / 3);
             } else if (maximumRowsOrColumns == 2) { // medium
-                iWidth = (width - (2 * margin)) / 2;
+                iWidth = width / maximumRowsOrColumns;
                 itemHeight = Math.Floor(height / 2);
             } else { // large
-                iWidth = (width - (2 * margin)) / 3;
+                iWidth = width / maximumRowsOrColumns;
                 itemHeight = height;
+                if (iWidth > 450) {
+                    iWidth = width / 4;
+                    MaximumRowsOrColumns = 4;
+                }
             }
 
+            ColumnSpan = maximumRowsOrColumns % 2 == 0 ? 2 : 1;
             ItemWidth = Math.Floor(iWidth);
-            //gridItems.ItemHeight = itemHeight;
-            //gridItems.ItemWidth = itemWidth;
+        }
+    }
+
+    public class VariableSizedGridView : Windows.UI.Xaml.Controls.GridView
+    {
+        protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
+        {
+            try {
+                var localItem = item as FrameworkElement;
+                if(localItem != null && localItem.Tag.ToString() == "Wide")
+                //element.SetValue(Windows.UI.Xaml.Controls.VariableSizedWrapGrid.RowSpanProperty, localItem.RowSpan);
+                element.SetValue(Windows.UI.Xaml.Controls.VariableSizedWrapGrid.ColumnSpanProperty, 2);
+            } catch {
+                //element.SetValue(Windows.UI.Xaml.Controls.VariableSizedWrapGrid.RowSpanProperty, 1);
+                element.SetValue(Windows.UI.Xaml.Controls.VariableSizedWrapGrid.ColumnSpanProperty, 1);
+            }
+
+            base.PrepareContainerForItemOverride(element, item);
         }
     }
 }
