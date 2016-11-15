6c9a791f6f0ffc8e3b184558c98a437257875045
Johan Lindfors <johan.lindfors@coderox.se>
2016-11-15 11:39:29 +0100

    Flexible layout of griditems

--- Client.Library/Views/SeventhPage.xaml.cs
+++ Client.Library/Views/SeventhPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -13,18 +14,54 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace Client.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class SeventhPage : Page
+    public sealed partial class SeventhPage : INotifyPropertyChanged
     {
         public SeventhPage()
         {
             this.InitializeComponent();
+            this.DataContext = this;
+
+            SizeChanged += OnSizeChanged;
+        }
+
+        private double itemWidth = 200;
+        public double ItemWidth
+        {
+            get { return itemWidth; }
+            set {
+                itemWidth = value;
+                if(PropertyChanged != null) {
+                    PropertyChanged(this, new PropertyChangedEventArgs("ItemWidth"));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var width = e.NewSize.Width;
+            var height = e.NewSize.Height - appBar.ActualHeight;
+
+            var margin = width <= 640 ? 12 : 24;
+            double itemWidth = 0;
+            double itemHeight = 0;
+            if (width < 641) { // small
+                itemWidth = width - (2 * margin);
+                itemHeight = Math.Floor(height / 3);
+            } else if (width < 1008) { // medium
+                itemWidth = (width - (2 * margin)) / 2;
+                itemHeight = Math.Floor(height / 2);
+            } else { // large
+                itemWidth = (width - (2 * margin)) / 3;
+                itemHeight = height;
+            }
+
+            ItemWidth = itemWidth;
+            //gridItems.ItemHeight = itemHeight;
+            //gridItems.ItemWidth = itemWidth;
         }
     }
 }
