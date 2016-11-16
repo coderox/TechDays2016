875beaf0e6c2fb8a56491cff633e5bd96428be7d
Johan Lindfors <johan.lindfors@coderox.se>
2016-11-16 14:17:20 +0100

    Now properly demonstrating three types of HamburgerMenu

---
+++ Client.BootStrapper/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace Client.UWP
+{
+    using Windows.ApplicationModel.Activation;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+
+    public sealed partial class App
+    {
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            var rootFrame = new Frame();
+
+            Window.Current.Content = rootFrame;
+
+            // rootFrame.Navigate(typeof(Views.UWPCommunityToolkitPage), e.Arguments);
+            rootFrame.Navigate(typeof(Views.NativePage), e.Arguments);
+
+            Window.Current.Activate();
+        }
+    }
+}
---
+++ Client.T10BootStrapper/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace Client.UWP
+{
+    using System.Threading.Tasks;
+    using Template10.Controls;
+    using Windows.ApplicationModel.Activation;
+    using Windows.UI.Xaml;
+
+    public sealed partial class App
+    {
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        public override UIElement CreateRootElement(IActivatedEventArgs e)
+        {
+            var service = this.NavigationServiceFactory(BackButton.Attach, ExistingContent.Exclude);
+            return new ModalDialog {
+                DisableBackButtonWhenModal = true,
+                Content = new Views.Template10Page(service)
+            };
+        }
+
+        public override Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
+        {
+            return this.NavigationService.NavigateAsync(typeof(Views.MainPage));
+        }
+    }
+}
